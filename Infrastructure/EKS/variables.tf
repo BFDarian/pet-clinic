029f34bd8bd71beb29f5ba87b9d52b0973f7ea66
BFDarian <bensfd99@gmail.com>
2021-10-28 23:18:43 +0100

    terra changes

--- Infrastructure/EKS/variables.tf
+++ Infrastructure/EKS/variables.tf
@@ -1,6 +1,6 @@
-variable "subnet_id_eks_pub_1" {}
-variable "subnet_id_eks_pub_2" {} 
-variable "subnet_id_eks_pub_3" {}
-variable "instance_type" {}
-variable "vpc_id" {}
-variable "security_group" {}
+# variable "subnet_id_eks_pub_1" {}
+# variable "subnet_id_eks_pub_2" {} 
+# variable "subnet_id_eks_pub_3" {}
+# variable "instance_type" {}
+# variable "vpc_id" {}
+# variable "security_group" {}
